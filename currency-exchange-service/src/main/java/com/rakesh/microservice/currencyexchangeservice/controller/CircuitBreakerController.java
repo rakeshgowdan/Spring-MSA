4de1dfd1b59bb4cd3bc146dccd0b0226f4dc22bf
Rakesh Gowda <rakeshgowda3101@gmail.com>
2022-04-20 18:45:27 +0530

    Implementing Circuit breaker

--- currency-exchange-service/src/main/java/com/rakesh/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/rakesh/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,10 +1,13 @@
 package com.rakesh.microservice.currencyexchangeservice.controller;
 
+import java.util.Date;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,7 +19,16 @@ public class CircuitBreakerController {
 	@GetMapping("/sample-cb")
 	@Retry(name="sample-api",fallbackMethod="fallBackResoponse")
 	public String sampleAPI() {
-		log.info("CB | Sample cb called ");
+		log.info("CB | Sample cb called "+ new Date());
+		
+		ResponseEntity<String> res= new RestTemplate().getForEntity("http://localhost:8000/", String.class);
+		return res.getBody();
+	}
+	
+	@GetMapping("/sample-cb1")
+	@CircuitBreaker(name="sample-api",fallbackMethod="fallBackResoponse")
+	public String sampleCBAPI() {
+		log.info("CB | Sample cb called "+ new Date());
 		
 		ResponseEntity<String> res= new RestTemplate().getForEntity("http://localhost:8000/", String.class);
 		return res.getBody();
